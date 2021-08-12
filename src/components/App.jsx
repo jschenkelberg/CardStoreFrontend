009ddbd5c0c9ba109350774c78df206c1dd93f07
Mr-Nelson <kdnels88@gmail.com>
2021-08-12 15:19:09 -0500

    added logic into App.jsx component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,59 @@ import React,{Component} from 'react';
 import './App.css';
 import TitleBar from './TitleBar/titlebar';
 import ShoppingCart from './ShoppingCart/shoppingCart';
+import axios from 'axios';
 
 class App  extends Component {
   constructor(props) {
       super(props);
+      this.state = {
+        user: [],
+        shoppingCart: [],
+        items: []
+      }
   }
+
+  componentDidMount(){
+    this.getAllItems();
+  }
+  getUser = async (event) => {
+    var res = await axios (`https://localhost:44394/api/user/${event}`)
+    return(
+      this.setState({
+        user: res.data
+      })
+    )
+  }
+
+
+  getShoppingCart = async (event) => {
+    var res = await axios (`https://localhost:44394/api/shoppingcart/${event}`)
+    return(
+      this.setState({
+        shoppingCart: res.data
+      })
+    )
+  }
+
+  getAllItems = async () => {
+    var res = await axios (`https://localhost:44394/api/merches`)
+    console.log(res);
+    return(
+      this.setState({
+        items: res.data
+      })
+    );
+  }
+
+  
+
+  delete
 render() {
   return (
     <React.Fragment>
       <TitleBar />
       <ShoppingCart/>
+      
       <h1>Our React App</h1>
     </React.Fragment>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { render } from '@testing-library/react';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'; 
 import React from 'react';
 import reactDom from 'react-dom';
 import App from './components/App';
 const jsxElement = <h1>Our React App</h1>;
 console.log(jsxElement);
 
-reactDom.render(<App />, document.getElementById('root'));
+reactDom.render(<Router><App /></Router>, document.getElementById('root'));
