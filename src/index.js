e9401a2095cc678fd36ea8757746580228992646
jamesburgess44 <jamesburgess44@yahoo.com>
2021-08-12 12:52:04 -0600

    removed the files we didnt need that were auto created and made a base index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const jsxElement = <h1>Our React App</h1>;
+console.log(jsxElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reactDom.render(jsxElement, document.getElementById('root'));
